7b2956a41eacbfbbca2c9b25fa4770c5c1c3228c
JuniperGamer <122415988+JuniperGamer@users.noreply.github.com>
2023-10-23 11:42:48 +1100

    Ember#: Added some new MALlib features

--- Ember.py
+++ Ember.py
@@ -1,9 +1,25 @@
 import re
+import os
 
 # Welcome to Ember!
-# Ember detects and removes code based off code given
+# Ember detects and removes code based on given patterns
 
 # This is a framework and not an actual program.
+
+def load_custom_patterns_from_file(file_path):
+    custom_patterns = []
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as file:
+            custom_patterns = [line.strip() for line in file.readlines()]
+    return custom_patterns
+
+def create_malicious_library():
+    folder = 'malicious_library'
+    file_path = os.path.join(folder, 'MALlib.txt')
+
+    os.makedirs(folder, exist_ok=True)
+    with open(file_path, 'w') as file:
+        file.write('# Add your custom malicious patterns here, one per line')
 
 def scan_for_malicious_code(code):
     # Define patterns for common malicious code
@@ -13,13 +29,19 @@ def scan_for_malicious_code(code):
         r'exec\(',
         r'os\.system',
     ]
-    
+
+    # Load custom malicious patterns from the file
+    custom_patterns = load_custom_patterns_from_file('malicious_library/MALlib.txt')
+
+    # Add custom patterns to the list
+    malicious_patterns.extend(custom_patterns)
+
     for pattern in malicious_patterns:
         if re.search(pattern, code):
             print("Potential malicious code found:")
             print(code)
             return True
-    
+
     return False
 
 def remove_malicious_code(code):
@@ -28,8 +50,12 @@ def remove_malicious_code(code):
     cleaned_code = re.sub(r'system\("', '', cleaned_code)
     cleaned_code = re.sub(r'exec\(', '', cleaned_code)
     cleaned_code = re.sub(r'os\.system', '', cleaned_code)
-    
+
     return cleaned_code
+
+# Create the malicious library if it doesn't exist
+if not os.path.exists('malicious_library/MALlib.txt'):
+    create_malicious_library()
 
 # Sample code to be scanned
 code_to_scan = """
@@ -43,3 +69,4 @@ if scan_for_malicious_code(code_to_scan):
     print(cleaned_code)
 else:
     print("No malicious code detected.")
+
